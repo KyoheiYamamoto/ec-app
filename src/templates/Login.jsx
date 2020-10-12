6a559d21c0987ae3ce5827cc4f50dc38cec587e6
KyoheiYamamoto <arienkyohei@gmail.com>
2020-10-12 22:56:13 +0900

    re-ducksパターンの勉強

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { push } from "connected-react-router";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
+import { signInAction } from "../reducks/users/actions";
 
 const Login = () => {
-  const dispatch = useDispatch()
-  const selector = useSelector(state => state);
+  const dispatch = useDispatch();
 
-  console.log(selector.router);
-  
   return (
     <div>
       <h2>ログイン</h2>
-      <button onClick={() =>dispatch(push('/'))}>
+      <button
+        onClick={() => {
+          dispatch(signInAction({ uid: '0001', username: 'torahack' }));
+          dispatch(push('/'));
+        }}
+      >
         ログインする
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
